509caa7fbdad4537b4e3c662fb3142bd300444d4
Arneelus Lewis <arneelus777@gmail.com>
2020-04-14 16:31:35 -0400

    Created the users pages

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,28 @@ Route::get('/about', 'StaticPagesController@about');
 Route::get('/reservations', 'StaticPagesController@reservations');
 Route::get('/contact', 'StaticPagesController@contact');
 Route::get('/offers', 'StaticPagesController@offers');
+
 // Admin Dashboard
 Route::get('/admin', 'admin\AdminController@dashboard');
+
 // Admin Food Categories
 Route::get('/admin/food-categories', 'admin\FoodCategoriesController@index');
 Route::get('/admin/food-categories/create', 'admin\FoodCategoriesController@create');
 Route::get('/admin/food-categories/{id}/edit', 'admin\FoodCategoriesController@edit');
-// Admin Customers
-Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers');
-Route::get('/admin/reservations', 'admin\CustomersController@allReservations');
+
 // Admin Food Items
 Route::get('/admin/food-items', 'admin\FoodItemsController@index');
 Route::get('/admin/food-items/create', 'admin\FoodItemsController@create');
 Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
+
+// Admin Users
+Route::get('/admin/users', 'admin\UsersController@index');
+Route::get('/admin/users/create', 'admin\UsersController@create');
+Route::get('/admin/users/{id}/edit', 'admin\UsersController@edit');
+
+// Admin Customers
+Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers');
+Route::get('/admin/reservations', 'admin\CustomersController@allReservations');
 
 // Admin Authentication
 Route::get('/admin/register', function () {
